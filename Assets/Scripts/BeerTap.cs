02b4053fddb7be5d518b5ce7a566649719601712
jymyte <jyrimy@gmail.com>
2021-04-23 18:25:42 +0300

    Table Prefab
    Player inventory
    Take beer from counter and serve it to table

--- Assets/Scripts/BeerTap.cs
+++ Assets/Scripts/BeerTap.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class BeerTap : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject player;
+    Player playerScripti;
+
+    private void Start() {
+        playerScripti = player.GetComponent<Player>();
+    }
+
     private void OnMouseDown() {
         Debug.Log("Beer Tap");
+        playerScripti.AddBeer();
     }
 }
