393c9204fce64c95f1b1496bd677a9b6d25274af
jymyte <jyrimy@gmail.com>
2021-06-17 18:11:24 +0300

    Menu scene ready for graphics.
    MusicController for muting music.

---
+++ Assets/Scripts/Controllers/MenuController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public void StartGame() {
+      SceneFader.instance.LoadScene("Release");
+    }
+
+    public void MusicOnOff () {
+         if (MusicController.instance.audioSource.isPlaying == true) {
+            MusicController.instance.PlayMusic(false);
+        } else {
+            MusicController.instance.PlayMusic(true);
+        }
+    }
+}
